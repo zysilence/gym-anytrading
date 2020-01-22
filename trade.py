0375887872b4f55c5d4c348837303938005c2b7d
zysilence <zysilence329b@gmail.com>
2020-01-22 17:54:34 +0800

    add tensorboard summary for profit and total reward

--- trade.py
+++ trade.py
@@ -14,7 +14,7 @@ import tensorflow as tf
 import numpy as np
 
 
-def custom_cnn(scaled_images, **kwargs):
+def nature_cnn(scaled_images, **kwargs):
     """
     CNN from Nature paper.
 
@@ -23,11 +23,27 @@ def custom_cnn(scaled_images, **kwargs):
     :return: (TensorFlow Tensor) The CNN output layer
     """
     activ = tf.nn.relu
-    layer_1 = activ(conv(scaled_images, 'c1', n_filters=32, filter_size=(8, 1), stride=4, init_scale=np.sqrt(2), **kwargs))
-    layer_2 = activ(conv(layer_1, 'c2', n_filters=64, filter_size=(4, 1), stride=2, init_scale=np.sqrt(2), **kwargs))
-    layer_3 = activ(conv(layer_2, 'c3', n_filters=64, filter_size=(3, 1), stride=1, init_scale=np.sqrt(2), **kwargs))
+    layer_1 = activ(conv(scaled_images, 'c1', n_filters=32, filter_size=8, stride=4, init_scale=np.sqrt(2), **kwargs))
+    layer_2 = activ(conv(layer_1, 'c2', n_filters=64, filter_size=4, stride=2, init_scale=np.sqrt(2), **kwargs))
+    layer_3 = activ(conv(layer_2, 'c3', n_filters=64, filter_size=3, stride=1, init_scale=np.sqrt(2), **kwargs))
     layer_3 = conv_to_fc(layer_3)
     return activ(linear(layer_3, 'fc1', n_hidden=512, init_scale=np.sqrt(2)))
+
+
+def custom_cnn(scaled_images, **kwargs):
+    """
+    Customized CNN.
+
+    :param scaled_images: (TensorFlow Tensor) Image input placeholder
+    :param kwargs: (dict) Extra keywords parameters for the convolutional layers of the CNN
+    :return: (TensorFlow Tensor) The CNN output layer
+    """
+    activ = tf.nn.relu
+    layer_1 = activ(conv(scaled_images, 'c1', n_filters=16, filter_size=(5, 1), stride=4, init_scale=np.sqrt(2), **kwargs))
+    layer_2 = activ(conv(layer_1, 'c2', n_filters=16, filter_size=(4, 1), stride=2, init_scale=np.sqrt(2), **kwargs))
+    layer_3 = activ(conv(layer_2, 'c3', n_filters=32, filter_size=(3, 1), stride=1, init_scale=np.sqrt(2), **kwargs))
+    layer_3 = conv_to_fc(layer_3)
+    return activ(linear(layer_3, 'fc1', n_hidden=64, init_scale=np.sqrt(2)))
 
 
 class CustomCnnPolicy(FeedForwardPolicy):
@@ -35,6 +51,25 @@ class CustomCnnPolicy(FeedForwardPolicy):
         super(CustomCnnPolicy, self).__init__(sess, ob_space, ac_space, n_env, n_steps, n_batch, reuse,
                                               cnn_extractor=custom_cnn,
                                               feature_extraction="cnn", **_kwargs)
+
+
+def callback(locals_, globals_):
+    self_ = locals_['self']
+    """
+    # Log additional tensor
+    if not self_.is_tb_set:
+        with self_.graph.as_default():
+            tf.summary.scalar('value_target', tf.reduce_mean(self_.value_target))
+            self_.summary = tf.summary.merge_all()
+        self_.is_tb_set = True
+    """
+    # Log scalar value (here a random variable)
+    total_reward = env._total_reward
+    total_profit = env._total_profit
+    summary = tf.Summary(value=[tf.Summary.Value(tag='total reward', simple_value=total_reward),
+                                tf.Summary.Value(tag='total profit', simple_value=total_profit)])
+    locals_['writer'].add_summary(summary, self_.num_timesteps)
+    return True
 
 
 if __name__ == '__main__':
@@ -54,7 +89,7 @@ if __name__ == '__main__':
     observation = env.reset()
     model = PPO2(CustomCnnPolicy, env, verbose=0, tensorboard_log="./tensorboard_log/")
     # model = DQN(MlpPolicy, env, verbose=1)
-    model.learn(total_timesteps=10000, tb_log_name="second_run")
+    model.learn(total_timesteps=500000, tb_log_name="PPO2_Cnn", callback=callback)
 
     observation = env.reset()
     # Test
